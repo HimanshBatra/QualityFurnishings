4b1dd33cedaa813e1d45b87284c1897ebd5040e7
HimanshBatra <batrah91@gmail.com>
2021-07-02 02:38:50 -0400

    user log out

--- app/src/main/java/com/example/qualityfurnishings/fragment/UserMenuFragment.java
+++ app/src/main/java/com/example/qualityfurnishings/fragment/UserMenuFragment.java
@@ -1,5 +1,10 @@
 package com.example.qualityfurnishings.fragment;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +12,71 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.qualityfurnishings.R;
+import com.example.qualityfurnishings.activity.LoginActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UserMenuFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class UserMenuFragment extends Fragment {
+    LinearLayout MyOrders,LogOut;
+    public static final String MyPREFERENCES = "LoginPref" ;
+    public static final String UserType = "usertype";
+    SharedPreferences sharedpreferences;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public UserMenuFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UserMenuFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UserMenuFragment newInstance(String param1, String param2) {
-        UserMenuFragment fragment = new UserMenuFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_menu, container, false);
+        View view = inflater.inflate(R.layout.fragment_user_menu, container, false);
+        MyOrders = (LinearLayout)view.findViewById(R.id.myorders);
+        LogOut = (LinearLayout)view.findViewById(R.id.logout);
+        LogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder
+                        = new AlertDialog
+                        .Builder(getContext());
+                builder.setMessage("Are you sure you want to LogOut?");
+
+                // Set Alert Title
+                builder.setTitle("Alert !");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(getActivity(), LoginActivity.class);
+                        sharedpreferences = getActivity().getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedpreferences.edit();
+                        editor.putString(UserType, "");
+                        editor.commit();
+                        startActivity(intent);
+
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        getFragmentManager().beginTransaction().replace(R.id.mainContainer,new UserMenuFragment()).commit();
+                    }
+                });
+                builder.show();
+            }
+        });
+        return view;
     }
 }
