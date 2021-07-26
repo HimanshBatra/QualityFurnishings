bc0de0349e4d539025b098b3e1500dbf85440216
HimanshBatra <batrah91@gmail.com>
2021-07-25 20:46:53 -0400

    search functionality

--- app/src/main/java/com/example/qualityfurnishings/activity/UserSearchScreen.java
+++ app/src/main/java/com/example/qualityfurnishings/activity/UserSearchScreen.java
@@ -8,6 +8,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.qualityfurnishings.R;
 import com.example.qualityfurnishings.adapter.UserSearchAdapter;
@@ -24,7 +29,11 @@ import java.util.List;
 public class UserSearchScreen extends AppCompatActivity {
     private RecyclerView recyclerView;
     String Category;
+    ImageView Imgsearch;
+    EditText searchProduct;
+    String flag="false";
     List<ProductModal> productList;
+    ArrayList<ProductModal> searchProductList;
     UserSearchAdapter userSearchAdapter;
     public static Context context;
 
@@ -34,12 +43,53 @@ public class UserSearchScreen extends AppCompatActivity {
         setContentView(R.layout.activity_user_search_screen);
         context = getApplicationContext();
         recyclerView = findViewById(R.id.productsView);
+        Imgsearch=(ImageView)findViewById(R.id.search);
+        searchProduct=findViewById(R.id.etSearch);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
         Intent i = getIntent();
         Bundle b = i.getExtras();
         Category=b.getString("category");
+        loadProducts();
+        Imgsearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchvalue=searchProduct.getText().toString();
+                flag="false";
+                if(!TextUtils.isEmpty(searchvalue)){
+                    searchProductList=new ArrayList<>();
+                    for(int i=0;i<productList.size();i++){
+                        if (productList.get(i).getName().toLowerCase().contains(searchvalue.trim().toLowerCase())){
+                            flag="true";
+                            searchProductList.add(new ProductModal(productList.get(i).getImage(), productList.get(i).getName(),productList.get(i).getDescription(),
+                                    productList.get(i).getQuality(),productList.get(i).getQuantity(),productList.get(i).getPrice(),productList.get(i).isSale(),
+                                    productList.get(i).getDiscount(), productList.get(i).getId(),Category,productList.get(i).getSubCategory()));
+                            userSearchAdapter = new UserSearchAdapter(searchProductList, UserSearchScreen.this);
+                            recyclerView.setAdapter(userSearchAdapter);
+
+
+                        }
+
+                    }
+
+                    if(flag.equals("false")){
+                        searchProductList.clear();
+                        userSearchAdapter = new UserSearchAdapter(searchProductList, UserSearchScreen.this);
+                        recyclerView.setAdapter(userSearchAdapter);
+
+                    }
+
+                }
+                else{
+                    loadProducts();
+                }
+            }
+        });
+
+    }
+
+    private void loadProducts() {
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
         databaseReference.child("FurnitureCategory").child(Category).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -48,7 +98,9 @@ public class UserSearchScreen extends AppCompatActivity {
                 for (DataSnapshot dataSnapshot : snapshot.getChildren()){
                     for (DataSnapshot itemList : dataSnapshot.getChildren()){
                         ProductModal listData = itemList.getValue(ProductModal.class);
-                        productList.add(new ProductModal(listData.getImage(), listData.getName(),listData.getDescription(),listData.getQuality(),listData.getQuantity(),listData.getPrice(),listData.isSale(),listData.getDiscount(),itemList.getKey(),Category,dataSnapshot.getKey()));
+                        productList.add(new ProductModal(listData.getImage(), listData.getName(),listData.getDescription(),
+                                listData.getQuality(),listData.getQuantity(),listData.getPrice(),listData.isSale(),
+                                listData.getDiscount(),itemList.getKey(),Category,dataSnapshot.getKey()));
 
                     }
                 }
