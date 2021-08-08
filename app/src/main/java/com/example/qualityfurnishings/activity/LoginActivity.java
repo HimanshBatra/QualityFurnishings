43d44cc0810ff4519f5a6256783a469318f30a7b
HimanshBatra <batrah91@gmail.com>
2021-08-08 02:22:34 -0400

    in activity back button bug resolve

--- app/src/main/java/com/example/qualityfurnishings/activity/LoginActivity.java
+++ app/src/main/java/com/example/qualityfurnishings/activity/LoginActivity.java
@@ -2,6 +2,9 @@ package com.example.qualityfurnishings.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -12,6 +15,30 @@ import com.example.qualityfurnishings.fragment.UserLoginFragment;
 
 public class LoginActivity extends AppCompatActivity {
 //    TextView user,admin;
+public void onBackPressed(){
+    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+    builder.setTitle("Quit");
+    builder.setMessage("Do You want to close the application.");
+
+    // add a button
+    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            finishAffinity();
+
+        }
+    });
+    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            return;
+
+        }
+
+    });
+    builder.show();
+
+}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
